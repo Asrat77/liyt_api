d631558914369ae15be71908e106ee667a491abb
Asrat <asratextras77@gmail.com>
2024-11-11 13:18:43 +0300

    Add address translation to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
+  def primary_address=(coordinates)
+      self[:primary_address] = { "latitude" => coordinates[0], "longitude" => coordinates[1] }
+    end
 
+    # Set the secondary_address as a hash with latitude and longitude
+    def secondary_address=(coordinates)
+      self[:secondary_address] = { "latitude" => coordinates[0], "longitude" => coordinates[1] }
+    end
 end
