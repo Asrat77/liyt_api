c59838e0a5d5b91d152aa3bcc396e47d4b61c63a
Asrat <asratextras77@gmail.com>
2024-11-13 21:11:45 +0300

    Calculate order price

--- app/services/order_service.rb
+++ app/services/order_service.rb
@@ -3,4 +3,37 @@ class OrderService
     api_key = ENV["GEBETA_API_KEY"]
     result = HTTParty.get("https://mapapi.gebeta.app/api/v1/route/geocoding?name=#{name}&apiKey=#{api_key}")
   end
+
+  def calculate_price(origin, destination)
+    distance_meters, time_taken_seconds = get_distance_and_time(origin, destination)
+    distance_km = distance_meters / 1000.0
+    rate_per_km = 5.0
+    time_rate = 0.05
+    total_price = (distance_km * rate_per_km) + (time_taken_seconds * time_rate)
+    total_price
+  end
+
+  def get_distance_and_time(origin, destination)
+    # Extract latitude and longitude from the origin and destination
+    origin_lat, origin_lon = parse_location(origin)
+    destination_lat, destination_lon = parse_location(destination)
+
+    # Call the new API to get distance and time
+    api_key = ENV["GEBETA_API_KEY"]
+    response = HTTParty.get("https://mapapi.gebeta.app/api/v1/route/driving/direction/?la1=#{origin_lat}&lo1=#{origin_lon}&la2=#{destination_lat}&lo2=#{destination_lon}&apiKey=#{api_key}")
+    debugger
+
+    # Parse the response
+    total_distance = response.parsed_response["totalDistance"] # Adjust based on actual response structure
+    time_taken = response.parsed_response["timetaken"] # Adjust based on actual response structure
+    [total_distance, time_taken] # Return both values
+  end
+
+  private
+
+  def parse_location(location)
+    # Assuming location is a string in the format "latitude,longitude"
+    lat, lon = location.split(',').map(&:strip)
+    [lat, lon]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  resources :orders
+  resources :orders do
+    collection do
+      get 'get_price', to: 'orders#get_price'
+    end
+  end
   resources :drivers
   resources :users
   post "/login", controller: :access, action: :login
   post "/signup", controller: :access, action: :signup
   get "/location/:name", to: "orders#location", as: "location"
+  get "/get_price/:origin/:destination", to: "orders#get_price", as: "get_price"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
