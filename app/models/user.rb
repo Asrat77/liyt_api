0b28fd00207e9540d83cb26cdda76916ea6443b8
Asrat <asratextras77@gmail.com>
2024-11-14 12:11:25 +0300

    Additional fields for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,15 @@ class User < ApplicationRecord
   validates :phone_number, presence: true
   validates :business_name, presence: true
   validates :email, presence: true, uniqueness: true
+  validates :license_plate_number, uniqueness: true, allow_nil: true
+
+  enum vehicle_type: { car: 0, motorcycle: 1 }
 
   def primary_address=(coordinates)
     if coordinates.is_a?(Hash) && coordinates["latitude"].present? && coordinates["longitude"].present?
       self[:primary_address] = { "latitude" => coordinates["latitude"], "longitude" => coordinates["longitude"] }
     else
-    self[:primary_address] = { "latitude" => nil, "longitude" => nil }
-
+      self[:primary_address] = { "latitude" => nil, "longitude" => nil }
     end
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,4 +15,27 @@ RSpec.describe User, type: :model do
     user = build(:user, email: nil)
     expect(user).not_to be_valid
   end
+
+  
+
+  it "validates that license_plate_number is unique" do
+    user1 = create(:user, license_plate_number: "ABC123")
+    user2 = build(:user, license_plate_number: "ABC123")
+    expect(user2).not_to be_valid
+  end
+
+  it "defaults is_driver to false" do
+    user = create(:user)
+    expect(user.is_driver).to be_falsey
+  end
+
+  it "can set vehicle_type to car" do
+    user = create(:user, vehicle_type: :car)
+    expect(user.vehicle_type).to eq("car")
+  end
+
+  it "can set vehicle_type to motorcycle" do
+    user = create(:user, vehicle_type: :motorcycle)
+    expect(user.vehicle_type).to eq("motorcycle")
+  end
 end
